7cb8b4f8f1c56987e3f8c89172189ee80424fca1
MeagherPatrick <pat.j.meagher1@gmail.com>
2024-03-24 15:18:13 -0400

    Added: User role

--- JFE/server/models/user/user.model.ts
+++ JFE/server/models/user/user.model.ts
@@ -4,19 +4,21 @@ import mongoose, { Document, Model } from 'mongoose';
 interface IUser extends Document {
   email: string;
   pods: string[];
+  role: string;
 }
 
 //schema for the User model
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   pods: { type: [String], required: false, default: [] },
+  role: { type: String, required: true, default: 'PENDING...' },
 });
 
 // Static methods for the User model
 UserSchema.statics.findOrCreate = async function ({ email }): Promise<IUser> {
   let user = await this.findOne({ email }).exec();
   if (!user) {
-    user = await this.create({ email });
+    user = await this.create({ email, role: 'PENDING...' });
   }
   return user;
 };
