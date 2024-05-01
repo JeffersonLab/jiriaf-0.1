74a6b491c2e72cd8d48fa107f4e7f1b8420b8c2d
MeagherPatrick <pmeag001@odu.edu>
2024-05-01 15:21:09 -0400

    Added deleteWorkflowFromDB and deleteAllWorkflowsByUser routes

--- JFE/server/routes/workflow/workflow.routes.ts
+++ JFE/server/routes/workflow/workflow.routes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { createWorkflow } from '../../controllers/workflow/workflow.controller';
+import { createWorkflow, deleteWorkflowFromDB, deleteAllWorkflowsByUser } from '../../controllers/workflow/workflow.controller';
 
 const router = express.Router();
 
 router.post('/workflows', createWorkflow);
+router.delete('/workflows', deleteWorkflowFromDB);
+router.delete('/workflows/delete-all-by-user', deleteAllWorkflowsByUser);
 
 export default router;
