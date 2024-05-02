224503602e1149e0bdca07d415d7def39014f95e
MeagherPatrick <pmeag001@odu.edu>
2024-05-02 17:22:39 -0400

    initial

---
+++ JFE/client/src/app/components/admin-panel/admin-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-panel.component.html',
+  styleUrl: './admin-panel.component.css'
+})
+export class AdminPanelComponent {
+
+}
