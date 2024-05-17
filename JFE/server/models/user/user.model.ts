a59745a625e0c7a8774cfa420b21e3390a36eef7
MeagherPatrick <pmeag001@odu.edu>
2024-05-17 14:00:08 -0400

    Debugged: Local instance of MongoDB was creating duplicate users with conflicting roles. Fixed: UserSchema now properly includes the static methods, ensuring they are recognized by TypeScript. Fixed: IUserModel interface now explicitly declares the static methods. Fixed: User model is now created with the type IUserModel to ensure TypeScript recognizes the static methods.

--- JFE/server/models/user/user.model.ts
+++ JFE/server/models/user/user.model.ts
@@ -1,3 +1,4 @@
+// ./models/user/user.model.ts
 import mongoose, { Document, Model } from 'mongoose';
 const roles = ['admin', 'user', 'guest', 'PENDING...'];
 // User interface extending mongoose.Document
@@ -7,9 +8,9 @@ interface IUser extends Document {
   role: string;
 }
 interface IUserModel extends Model<IUser> {
-  findOrCreate({ email }: { email: string }): Promise<IUser>;
-  findOneByEmail(email: string): Promise<IUser | null>;
-  build(attrs: IUser): IUser;
+  findOrCreate: ({ email }: { email: string }) => Promise<IUser>;
+  findOneByEmail: (email: string) => Promise<IUser | null>;
+  build: (attrs: IUser) => IUser;
 }
 
 //schema for the User model
@@ -42,6 +43,6 @@ UserSchema.statics.build = function (attrs: IUser) {
 UserSchema.index({ email: 1 });
 
 // Create the model from the schema
-const User: Model<IUser> = mongoose.model<IUser, IUserModel>('User', UserSchema);
-
+// const User: Model<IUser> = mongoose.model<IUser, IUserModel>('User', UserSchema);
+const User = mongoose.model<IUser, IUserModel>('User', UserSchema);
 export { User, IUser };
